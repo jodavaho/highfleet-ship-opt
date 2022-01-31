42732d3b196a86987f47bdc5336fe069cfac7145
Josh Vander Hook <josh@vanderhook.info>
2022-01-31 07:40:04 -0800

    Build files

--- src/py/hfopt.py
+++ src/py/hfopt.py
@@ -2,14 +2,13 @@ def __bootstrap__():
    global __bootstrap__, __loader__, __file__
    import sys, pkg_resources, imp
 
+   __file__ = pkg_resources.resource_filename(__name__,'libscip.so')
+   imp.load_dynamic(__name__,__file__)
+
    __file__ = pkg_resources.resource_filename(__name__,'libhf.so')
    imp.load_dynamic(__name__,__file__)
 
-   __file__ = pkg_resources.resource_filename(__name__,'libscip.so')
-
    __file__ = pkg_resources.resource_filename(__name__,'hfopt.cpython-38-x86_64-linux-gnu.so')
-   imp.load_dynamic(__name__,__file__)
-
    imp.load_dynamic(__name__,__file__)
 
    __loader__ = None; del __bootstrap__, __loader__
